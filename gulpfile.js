0fff985b273664c5cb823a149a0274979bdbf00f
Kim63895 <kim977563@gmail.com>
2024-08-09 14:27:05 +0200

    initial hw16 gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+
+const { watch, series } = require('gulp')
+var gulp = require('gulp');
+var cssnano = require('gulp-cssnano');
+const sass = require('gulp-sass')(require('sass'));
+
+// gulp.task()
+// gulp.src()
+// gulp.prependListener()
+// gulp.task()
+// gulp.watch()
+
+gulp.task('sass', function() {
+    return gulp.src('App/scss/style.scss')
+     .pipe(sass().on('error', sass.logError))
+    //  .pipe(cssnano())
+     .pipe(gulp.dest('dest/css'))
+}
+)
+// gulp.task('watch', function(){
+//     gulp.watch('app/scss/*.scss', ['sass'])
+
+// })
+gulp.task ('watch', function() {
+    watch('App/scss/*.scss', gulp.series('sass'))
+})
