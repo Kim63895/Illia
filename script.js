7eb3e02f440b7d368e2f5526666b8b524a345de2
Kim63895 <kim977563@gmail.com>
2024-11-03 19:23:30 +0100

    hw11

--- script.js
+++ script.js
@@ -1,59 +1,33 @@
 const form = document.querySelector("form");
-const message = document.getElementById("message");
 const nameInput = document.querySelector(".form-control");
-const erroMessage = document.querySelectorAll("span");
-const fillMassage = document.getElementById("exampleFormControlTextarea1");
-const fillTell = document.getElementById("exampleFormControlInput2");
+const errorMessage = document.querySelectorAll("span");
+const fillMessage = document.getElementById("exampleFormControlTextarea1");
+const fillPhone = document.getElementById("exampleFormControlInput2");
 const fillEmail = document.getElementById("exampleFormControlInput3");
 
-const checkName = (regExp, item) => {
-  if (regExp.test(nameInput.value)) {
-    item.textContent = "";
+const validateField = (input, regExp, errorMgElement, errorMessage) => {
+  if (regExp.test(input.value)) {
+    errorMgElement.textContent = ""; 
   } else {
-    item.textContent = "This sectioon need to be fill";
-    item.style.color = "red";
+    errorMgElement.textContent = errorMessage; 
+    errorMgElement.style.color = "red";
   }
 };
 
-const checkMessage = (regExp, item) => {
-  if (regExp.test(fillMassage.value)) {
-    item.textContent = "";
-  } else {
-    item.textContent = "This section need to have at list 5";
-    item.style.color = "red";
-  }
+const validateForm = () => {
+  validateField(nameInput, /^[A-Za-z\s]+$/, errorMessage[0], "This section needs to be filled with letters only.");
+  validateField(fillMessage, /^.{5,}$/, errorMessage[1], "This section needs at least 5 characters.");
+  validateField(fillPhone, /^\+380\d{9}$/, errorMessage[2], "This section needs to start with '+380' and have 9 digits.");
+  validateField(fillEmail,  /\S+@\S+\.\S+/, errorMessage[3], "This section needs a valid email format with '@' and '.'.");
 };
 
-const checkPhoneNumber = (regExp, item) => {
-  if (regExp.test(fillTell.value)) {
-    item.textContent = "";
-  } else {
-    item.textContent = "This section need to start '+380' ";
-    item.style.color = "red";
-  }
-};
+form.addEventListener("input", validateForm);
 
-const checkEmail = (regExp, item) => {
-  if (regExp.test(fillEmail.value)) {
-    item.textContent = "";
-  } else {
-    item.textContent = "This section need @ and '.' ";
-    item.style.color = "red";
-  }
-};
-
-const validateValue = () => {
-  checkName(/[A-Za-z]+/, erroMessage[0]);
-  checkMessage(/.{6}/, erroMessage[1]);
-  checkPhoneNumber(/^\+380\d{9}$/, erroMessage[2]);
-  checkEmail(/\S+@\S+\.S+/, erroMessage[3]);
-};
-form.addEventListener("input", validateValue);
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const formData = new FormData(event.target);
   const formObj = {};
   formData.forEach((value, key) => (formObj[key] = value));
-  console.log(formObj);
+  console.log(formObj); 
 });
