f61f1f0131c9d56415ba394c689767a484893b42
Kim63895 <kim977563@gmail.com>
2024-11-07 17:20:45 +0100

    hw12

--- javascript.js
+++ javascript.js
@@ -1,12 +1,19 @@
 function Slider(picturesSelector) {
   const slides = document.querySelectorAll(picturesSelector);
+  const dots = document.querySelectorAll(".dots li");
   let currentSlide = 0;
 
+  const showCurrentDot = () => {
+  dots.forEach((dot, index) => {
+    dot.style.backgroundColor = index === currentSlide ? "blue" : "";
+  });
+}
   const showSlide = () => {
     slides.forEach((slide, index) => {
       slide.style.display = index === currentSlide ? "block" : "none";
     });
     updateButtons();
+    showCurrentDot();
   };
   const updateButtons = () => {
     const prevButton = document.querySelector(".btn1");
@@ -15,24 +22,30 @@ function Slider(picturesSelector) {
     if (currentSlide === 0) {
       prevButton.style.visibility = "hidden";
     } else {
-      prevButton.style.visibility = "visible"; 
+      prevButton.style.visibility = "visible";
     }
 
     if (currentSlide === slides.length - 1) {
-      nextButton.style.visibility = "hidden"; 
+      nextButton.style.visibility = "hidden";
     } else {
-      nextButton.style.visibility = "visible"; 
+      nextButton.style.visibility = "visible";
     }
   };
-
-  this.nextSlide = function () {
-    currentSlide = (currentSlide + 1) % slides.length; 
+  this.dots = function (index) {
+    currentSlide = index;
     showSlide();
+    showCurrentDot();
+  };
+  this.nextSlide = function () {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide();
+    showCurrentDot();
   };
 
   this.prevSlide = function () {
     currentSlide = (currentSlide - 1 + slides.length) % slides.length;
     showSlide();
+    showCurrentDot();
   };
 
   this.init = function (prevButtonSelector, nextButtonSelector) {
@@ -42,6 +55,9 @@ function Slider(picturesSelector) {
     prevButton.addEventListener("click", this.prevSlide);
     nextButton.addEventListener("click", this.nextSlide);
 
+    dots.forEach((dot, index) => {
+      dot.addEventListener("click", () => this.dots(index));
+    });
     showSlide();
   };
 }
