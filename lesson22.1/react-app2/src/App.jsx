9b196b6bf242dbb12063ca75c42fdce8126d4b99
Kim63895 <kim977563@gmail.com>
2024-12-16 09:01:35 +0100

    hw22

---
+++ lesson22.1/react-app2/src/App.jsx
@@ -0,0 +1,14 @@
+import "./index.css";
+import Form from './components/Card';
+import Card from "./components/Form"; 
+
+const App = () => {
+  return (
+    <>
+      <Form />
+      <Card />
+    </>
+  );
+};
+
+export default App;
