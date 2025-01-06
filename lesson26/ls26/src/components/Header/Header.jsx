fad177726276bdd3359a774b7da04fa004a044e5
Kim63895 <kim977563@gmail.com>
2025-01-06 18:08:23 +0100

    hw26

--- lesson26/ls26/src/components/Header/Header.jsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux";
-import selectors from "../../redux/slice/selectors";
-
-const Header = () => {
-  const value = useSelector(selectors.counter.value);
-  return <header>Value: {value}</header>;
-};
-
-export default Header;
