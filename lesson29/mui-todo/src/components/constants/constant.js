0a8006516438e72a932e0ffacc68c603f5e54f2d
Kim63895 <kim977563@gmail.com>
2025-01-19 13:51:14 +0100

    hw29

---
+++ lesson29/mui-todo/src/components/constants/constant.js
@@ -0,0 +1,4 @@
+export const API = {
+  URL_SWAPI: "https://swapi.tech/api/",
+  URL_TODO: "https://678518331ec630ca33a72cbf.mockapi.io/todo",
+};
---
+++ lesson29/mui-todo/src/components/redux/slicesSwupi/swapiSlice.js
@@ -0,0 +1,61 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { API } from "../../constants/constant";
+import { fetchStart } from "../slices/todoSlice";
+
+const initialState = { items: [] };
+
+export const swapiSlice = createSlice({
+  name: "swapi",
+  initialState,
+  reducers: {
+    addItem: (state, action) => {
+      state.items.push(action.payload);
+    },
+    clearItems: (state) => {
+      state.items = [];
+    },
+    fetchStart: (state) => {
+      state.items = true;
+    },
+  },
+});
+export const { addItem, clearItems } = swapiSlice.actions;
+
+export const addSwapi = (text) => async (dispatch) => {
+  try {
+    const response = await fetch(`${API.URL_SWAPI}${text}`);
+
+    const data = await response.json();
+
+  dispatch(addItem({ id: +new Date(), text: data }));
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
+};
+
+export const deleteSwapi = (id) => async (dispatch) => {
+  dispatch(fetchStart());
+  try {
+    const response = await fetch(`${API.URL_SWAPI}${id}`, {
+      method: "DELETE",
+    });
+
+    const data = await response.json();
+
+     console.log("Response", data)
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
+};
+
+// export const getSwupi = () => async (dispatch) => {
+//   try {
+//     const response = await fetch(API.URL_SWUPI);
+
+//     const data = await response.json();
+
+//     dispatch(addItem({ id: data.id, text }));
+//   } catch (e) {
+//     console.error(e);
+//   }
+// };
