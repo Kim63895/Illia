1ff422500a2d89b8e35fe2ac7299ac2c9f406c01
Kim63895 <kim977563@gmail.com>
2025-01-20 14:17:13 +0100

    hw30

---
+++ lesson30/hw30/src/App.jsx
@@ -0,0 +1,13 @@
+import TodoForm from "./components/TodoForm/TodoForm";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <TodoForm />
+    </Provider>
+  );
+}
+
+export default App;
