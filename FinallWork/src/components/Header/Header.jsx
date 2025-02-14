b4bcf6fba4934c93ee8e5b13b15510499e697696
Kim63895 <kim977563@gmail.com>
2025-02-14 12:33:53 +0100

    FW

--- FinallWork/src/components/Header/Header.jsx
+++ FinallWork/src/components/Header/Header.jsx
@@ -1,12 +1,29 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import Icon from "../Icons/Icon";
+import { useNavigate } from "react-router-dom";
 
 
-export const Header = () => (
- 
+export const Header = () => {
+const navigate = useNavigate();
+  return (
+  <Box
+    sx={{
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      padding: "0 10px",
+    }}
+  >
     <Box sx={{ pl: "8px", pt: "8px", color: "white", bgcolor: "primary.main" }}>
       <Icon />
     </Box>
-  
-);
+    <Button
+      sx={{ color: "primary.main", bgcolor: "white", height: "20px" }}
+      onClick={() => navigate("/login")}
+    >
+      Login
+    </Button>
+  </Box>
+  )
+};
 export default Header;
